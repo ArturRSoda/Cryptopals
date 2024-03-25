1a0493229bd0f21f93a36bbf1ec8e4062e153edd
ArturRSoda <artursodar@gmail.com>
2024-03-25 15:19:52 -0300

    challange 21

--- Set3/utils.py
+++ Set3/utils.py
@@ -33,6 +33,78 @@ FREQ_TABLE = {
     'z' : 0.00074
 }
 
+
+class MT19937:
+    def __init__(self, version: int, seed: int):
+        assert version in (32, 64)
+        self.version = version
+
+        if (self.version == 32):
+            self.w = 32;         self.n = 624
+            self.m = 397;        self.r = 31
+            self.a = 0x9908B0DF; self.u = 11
+            self.d = 0xFFFFFFFF; self.s = 7
+            self.b = 0x9D2C5680; self.t = 15
+            self.c = 0xEFC60000; self.l = 18
+            self.f = 181243325
+        else:
+            self.w = 64;                 self.n = 312
+            self.m = 156;                self.r = 31
+            self.a = 0xB5026F5AA96619E9; self.u = 29
+            self.d = 0x5555555555555555; self.s = 17
+            self.b = 0x71D67FFFEDA60000; self.t = 37
+            self.c = 0xFFF7EEE000000000; self.l = 43
+            self.f = 636413622384679300
+
+        self.lower_mask = 0x7FFFFFFF
+        self.upper_mask = ~((1 << self.w) + self.lower_mask)
+            
+        self.MT = [i for i in range(self.n)]
+        self.index = self.n + 1
+        self.seed_mt(seed)
+
+    def seed_mt(self, seed: int):
+        self.index = self.n
+        self.MT[0] = seed
+        for i in range(1, self.n):
+            self.MT[i] = (self.f * (self.MT[i-1] ^ (self.MT[i-1] >> (self.w-2))) + i) & ((1 << self.w) - 1)
+ 
+    def genrand_int(self) -> int:
+        if self.index >= self.n:
+            self._twist()
+ 
+        y = self.MT[self.index]
+        y = y ^ ((y >> self.u) & self.d)
+        y = y ^ ((y << self.s) & self.b)
+        y = y ^ ((y << self.t) & self.c)
+        y = y ^ (y >> self.l)
+     
+        self.index += 1
+
+        return y & ((1 << self.w) - 1)
+
+    def get_state(self) -> list[int]:
+        return self.MT
+
+    def set_state(self, state: list[int]):
+        if (len(state) != self.n):
+            raise ValueError(f"State needs to be of size {self.n}")
+        self.index = self.n
+        self.MT = state
+
+    def _twist(self):
+        for i in range(self.n):
+            x = (self.MT[i] & self.upper_mask) + (self.MT[(i+1) % self.n] & self.lower_mask)
+            xA = x >> 1
+            if x % 2 != 0:
+                xA = xA ^ self.a
+            
+            self.MT[i] = self.MT[(i + self.m) % self.n] ^ xA
+    
+        self.index = 0
+
+
+
 def bytes_xor(b1: bytes, b2: bytes) -> bytes:
     return bytes(a^b for (a,b) in zip(b1, b2))
 
